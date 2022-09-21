ed8b7436fd122bba34657a8f74e8abb3e627e734
Kipchirchir Albert <albertkipchirchir02@gmail.com>
2022-09-21 05:49:21 +0300

    Updates

--- src/components/NewSession.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NewSession = () => {
-  return (
-    <div>NewSession</div>
-  )
-}
-
-export default NewSession
--- src/components/Signup.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Signup = () => {
-  return (
-    <div>Signup form</div>
-  )
-}
-
-export default Signup
